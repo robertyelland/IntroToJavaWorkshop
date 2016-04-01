100dd58cbd0e4e1a40dde0c09871871ec1d90ee8
TJHL <tjhl8545@gmail.com>
2016-04-01 14:55:07 -0700

    My last Day

--- src/day5/ScaryMaze.java
+++ src/day5/ScaryMaze.java
@@ -2,7 +2,6 @@ package day5;
 
 import java.applet.AudioClip;
 import java.awt.Dimension;
-
 import java.awt.Graphics;
 import java.awt.Robot;
 import java.awt.event.MouseEvent;
@@ -12,24 +11,28 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
+//import com.sun.media.jfxmedia.AudioClip;
+
 public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
-	
+
 	BufferedImage maze;
-	final int frameWidth = 600;
-	final int frameHeight = 400;
+	final int frameWidth = 800;
+	final int frameHeight = 600;
 
 	ScaryMaze() throws Exception {
-		//1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
-		maze = ImageIO.read(getClass().getResource("maze.png"));
-		//2. set the mouse pointer to the start of your maze using:
-		//new Robot().mouseMove(xPosition, yPosition)
-		
-		//3. add a mouse motion listener using:
-		//addMouseMotionListener(this)
-		
+		// 1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
+		maze = ImageIO.read(getClass().getResource("Maze.jpg"));
+		// 2. set the mouse pointer to the start of your maze using:
+		// new Robot().mouseMove(xPosition, yPosition)
+		new Robot().mouseMove(50, 300);
+		// 3. add a mouse motion listener using:
+		// addMouseMotionListener(this)
+		addMouseMotionListener(this);
+
 	}
 
 	@Override
@@ -37,29 +40,36 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseX = e.getX();
 		int mouseY = e.getY();
 		int mouseColor = maze.getRGB(mouseX, mouseY);
-		//4. print the mouseColor variable to see what color the mouse is touching
+		// 4. print the mouseColor variable to see what color the mouse is touching
+		System.out.println(mouseColor);
+		// 5. make a variable to hold the background color.
+		int Blue = -1;
+		// 6. if the mouse falls off the path (if it is on the background)
+		if (mouseColor == Blue) {
+			scare();
+		}
+		// call the scare method
 
-		//5. make a variable to hold the background color. 
+		// 10. if the mouse is on the end color
+		if (mouseColor == -14507256) {
+			JOptionPane.showMessageDialog(null, "GOOD JOB!");
+		}
 
-		//6. if the mouse falls off the path (if it is on the background)
-		
-				// call the scare method
-		
-		//10. if the mouse is on the end color
-				
-				// pop up a message to tell them they won
-		
+		// pop up a message to tell them they won
+
 	}
 
 	private void scare() {
 		System.out.println("BOO!");
-		//7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on freesound.org. Log in as leagueofamazing/code4life.
-		//AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
-		
-		//8. play the scary sound. Hint: type "sound" and then a period.		
-		
-		//9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
+		// 7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on
+		// freesound.org. Log in as leagueofamazing/code4life.
+		// AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource("Laugh.wav"));
 
+		// 8. play the scary sound. Hint: type "sound" and then a period.
+		sound.play();
+		// 9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
+		showScaryImage("Funny.jpg");
 	}
 
 	private void showScaryImage(String imageName) {
@@ -92,9 +102,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 	}
 
 	@Override
-	public void mouseDragged(MouseEvent e) {}
+	public void mouseDragged(MouseEvent e) {
+	}
 
 }
-
-
-
